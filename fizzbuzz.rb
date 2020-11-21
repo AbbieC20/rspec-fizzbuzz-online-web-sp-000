8f7052e52aa2cfb0da93dcbbd24371a550ce49ae
remote-torrent-6747 <abbyc@sky.com>
2020-11-21 16:14:41 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,8 @@ def fizzbuzz(int)
   if int % 3 == 0
     return "Fizz"
   elseif int % 5 == 0
-    puts "Buzz"
-  elseif int % 15 == 0
-    puts "Fizzbuzz"
-  else !int % 3 || !int % 5 == 0
-    puts ""
+    return "Buzz"
+  else int % 15 == 0
+    return "Fizzbuzz"
   end
 end
